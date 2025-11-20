0211d69c70a74c4cb42a4de153b07767fb113ebb
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-20 18:16:58 +0100

    change project architecture

--- CartItems/utils.py
+++ CartItems/utils.py
@@ -1,10 +1,36 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status , generics 
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import CreateModelMixin
-from .models import CartItems
-from .serializer import cartitems_serializer
-from Cartitems.models import CartItems 
 
+from .models import CartItems
+from Customer.models import Customer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+
+def get_the_current_session(self):
+        sessionID = self.request.session.session_key
+        if not sessionID:
+            sessionID = self.request.session.create()
+        return sessionID      
+def add_or_update_cartitems_for_guest_user(self,product,quantity):
+        cart_items = CartItems.objects.filter(product=product , guest_id=self.get_the_current_session())
+        if cart_items.exists():
+            cart_item = cart_items.first()
+            cart_item.quantity += quantity
+            cart_item.save()
+            return Response({'message :' : 'product quantity updated '}, status=status.HTTP_200_OK)
+        else:
+            cart_item = CartItems.objects.create(product=product , quantity=quantity , guest_id=self.get_the_current_session())
+            cart_item.save()
+            return Response({'message :' : 'product added to cart '}, status=status.HTTP_201_CREATED)
+        return cart_item
+def add_or_update_cartitems_for_customers(self, product, quantity):
+        customer = get_object_or_404(Customer, user=self.request.user)
+        cart_items = CartItems.objects.filter(product=product, customer=customer)
+        if cart_items.exists():
+            cart_item = cart_items.first()
+            cart_item.quantity += quantity
+            cart_item.save()
+            return Response({'message': 'Product quantity updated'}, status=status.HTTP_200_OK)
+        else:
+            cart_item = CartItems.objects.create(product=product, quantity=quantity, customer=customer)
+            cart_item.save()
+            return Response({'message': 'Product added to cart'}, status=status.HTTP_201_CREATED)
--- CartItems/views.py
+++ CartItems/views.py
@@ -7,7 +7,7 @@ from rest_framework.mixins import CreateModelMixin
 from .models import CartItems
 from .serializer import cartitems_serializer
 from Customer.models import Customer
-
+from .utils import add_or_update_cartitems_for_customers , add_or_update_cartitems_for_guest_user
 class AddToCartView(GenericAPIView,CreateModelMixin):
     serializer_class = cartitems_serializer
     def post(self,request,*args,**kwargs) :
@@ -20,36 +20,7 @@ class AddToCartView(GenericAPIView,CreateModelMixin):
             else:
                 cart_item = self.add_or_update_cartitems_for_guest_user(product,quantity)
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)   
-    def get_the_current_session(self):
-        sessionID = self.request.session.session_key
-        if not sessionID:
-            sessionID = self.request.session.create()
-        return sessionID      
-    def add_or_update_cartitems_for_guest_user(self,product,quantity):
-        cart_items = CartItems.objects.filter(product=product , guest_id=self.get_the_current_session())
-        if cart_items.exists():
-            cart_item = cart_items.first()
-            cart_item.quantity += quantity
-            cart_item.save()
-            return Response({'message :' : 'product quantity updated '}, status=status.HTTP_200_OK)
-        else:
-            cart_item = CartItems.objects.create(product=product , quantity=quantity , guest_id=self.get_the_current_session())
-            cart_item.save()
-            return Response({'message :' : 'product added to cart '}, status=status.HTTP_201_CREATED)
-        return cart_item
-    def add_or_update_cartitems_for_customers(self, product, quantity):
-        customer = get_object_or_404(Customer, user=self.request.user)
-        cart_items = CartItems.objects.filter(product=product, customer=customer)
-        if cart_items.exists():
-            cart_item = cart_items.first()
-            cart_item.quantity += quantity
-            cart_item.save()
-            return Response({'message': 'Product quantity updated'}, status=status.HTTP_200_OK)
-        else:
-            cart_item = CartItems.objects.create(product=product, quantity=quantity, customer=customer)
-            cart_item.save()
-            return Response({'message': 'Product added to cart'}, status=status.HTTP_201_CREATED)
-
+   
 class delete_product_cart(GenericAPIView , CreateModelMixin):
     def post(self , request , id):
         cart_items = get_object_or_404(CartItems , id=id)
